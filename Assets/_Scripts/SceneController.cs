944b818b603c5a9bf074ae6cd104e4670875f1d9
Alexnor11 <advance114@gmail.com>
2026-01-01 20:37:24 +0300

    5.3.3. Card instances

--- Assets/_Scripts/SceneController.cs
+++ Assets/_Scripts/SceneController.cs
@@ -4,12 +4,39 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
+    public const int greidRows = 2;
+    public const int greidCols = 4;
+    public const float offsetX = 2f;
+    public const float offsetY = 2.5f;
+
+    
     [SerializeField] MemoryCard originalCard;
     [SerializeField] Sprite[] images;
 
     private void Start()
     {
-        int id = Random.Range(0, images.Length);
-        originalCard.SerCard(id, images[id]);
+        Vector3 startPos = originalCard.transform.position;
+
+        for (int i = 0; i < greidCols; i++)
+        {
+            for (int j = 0; j < greidRows; j++)
+            {
+                MemoryCard card;
+                if(i == 0 && j == 0)
+                {
+                    card = originalCard;
+                }
+                else
+                {
+                    card = Instantiate(originalCard) as MemoryCard;
+                }
+                int id = Random.Range(0, images.Length);
+                originalCard.SerCard(id, images[id]);
+
+                float posX = (offsetX * i) + startPos.x;
+                float posY = (offsetY * j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY,startPos.z);
+            }
+        }        
     }
 }

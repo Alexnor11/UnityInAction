144b0f206e26a88da84b29d2709985f3a213c827
Alexnor11 <advance114@gmail.com>
2026-01-03 17:28:22 +0300

    Animation system

--- Assets/_Scripts/PlatformerPlayer.cs
+++ Assets/_Scripts/PlatformerPlayer.cs
@@ -7,10 +7,12 @@ public class PlatformerPlayer : MonoBehaviour
     public float speed = 4.5f;
 
     private Rigidbody2D body;
+    private Animator anim;
 
     private void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -18,5 +20,11 @@ public class PlatformerPlayer : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * speed;
         Vector2 movement = new Vector2(deltaX, body.velocity.y);
         body.velocity = movement;
+
+        anim.SetFloat("speed", Mathf.Abs(deltaX));
+        if (!Mathf.Approximately(deltaX, 0))
+        {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1); 
+        }
     }
 }

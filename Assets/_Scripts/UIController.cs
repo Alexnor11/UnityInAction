02053dbe4808e300aa47c6a276845ae9abe2cdee
Alexnor11 <advance114@gmail.com>
2026-01-05 14:49:54 +0300

    7.3.1. Programming an invisible UIController object

---
+++ Assets/_Scripts/UIController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] TMP_Text scoreLabel;
+
+    private void Update()
+    {
+        scoreLabel.text = Time.realtimeSinceStartup.ToString();
+    }
+
+    public void OnOpenSettings()
+    {
+        Debug.Log("open settings");
+    }
+}

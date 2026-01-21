348efea3f4bed622595ba1a2d529f19958056101
Alexnor11 <advance114@gmail.com>
2026-01-21 19:40:01 +0300

    Adding background music

--- Assets/_Scripts/Manager/AudioManager.cs
+++ Assets/_Scripts/Manager/AudioManager.cs
@@ -7,6 +7,26 @@ public class AudioManager : MonoBehaviour, IGameManager
     public ManagerStatus status {  get; private set; }
 
     [SerializeField] AudioSource soundSource;
+    [SerializeField] AudioSource music1Source;
+    
+    [SerializeField] string introBGMusic;
+    [SerializeField] string levelBGMusic;
+
+    private NetworkService network;
+
+    private float _musicVolume;
+    public float musicVolume
+    {
+        get { return _musicVolume; }
+        set
+        {
+            _musicVolume = value;
+            if (music1Source != null)
+            {
+                music1Source.volume = _musicVolume;
+            }
+        }
+    }
     
     public float soundVolume
     {
@@ -20,12 +40,34 @@ public class AudioManager : MonoBehaviour, IGameManager
         set { AudioListener.pause = value; }
     }
 
-    private NetworkService network;
+    public bool musicMute
+    {
+        get
+        {
+            if(music1Source != null)
+            {
+                return music1Source.mute;
+            }
+            return false;
+        }
+        set
+        {
+            if(music1Source == null)
+            {
+                music1Source.mute = value;
+            }
+        }
+    }
 
     public void Startup(NetworkService service)
     {
         network = service;
+
+        music1Source.ignoreListenerVolume = true;
+        music1Source.ignoreListenerPause = true;
+
         soundVolume = 1f;
+        musicVolume = 1f;
 
         status = ManagerStatus.Started;
     }
@@ -33,5 +75,26 @@ public class AudioManager : MonoBehaviour, IGameManager
     public void PlaySound(AudioClip clip)
     {
         soundSource.PlayOneShot(clip);
+    }
+
+    public void PlayIntroMusic()
+    {
+        PlayMusic(Resources.Load($"Music/{introBGMusic}") as AudioClip);
+    }
+
+    public void PlayLevelMusic()
+    {
+        PlayMusic(Resources.Load($"Music/{levelBGMusic}") as AudioClip);
+    }
+
+    private void PlayMusic(AudioClip clip)
+    {
+        music1Source.clip = clip;
+        music1Source.Play();
+    }
+
+    public void StopMusic()
+    {
+        music1Source.Stop();
     }
 }

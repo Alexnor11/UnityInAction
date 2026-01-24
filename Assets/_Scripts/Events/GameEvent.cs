a3b82c06e61e1a68497915f18e4d068ea92d59d9
Alexnor11 <advance114@gmail.com>
2026-01-24 17:50:27 +0300

    Replacing the old GUI with a new one

---
+++ Assets/_Scripts/Events/GameEvent.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// —ценарий GameEvent, который будет использоватьс€
+//с системой диспетчеров
+
+public static class GameEvent
+{
+   public const string HEALTH_UPDATED = "HEALTH_UPDATED";
+}

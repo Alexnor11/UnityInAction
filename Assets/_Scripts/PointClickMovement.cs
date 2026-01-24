c6f17b61377e4df2df6db319127b5814d396cf02
Alexnor11 <advance114@gmail.com>
2026-01-24 17:54:35 +0300

    Changing PointClickMovement

--- Assets/_Scripts/PointClickMovement.cs
+++ Assets/_Scripts/PointClickMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PointClickMovement : MonoBehaviour
 {
@@ -39,7 +40,7 @@ public class PointClickMovement : MonoBehaviour
     {
         Vector3 movement = Vector3.zero;
 
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //Направляем луч
                                                                          //в точку щелчка
